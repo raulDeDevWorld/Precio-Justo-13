96ce770f836fec60bad2bd1bdf54e92be467dd99
Your Name <you@example.com>
2023-06-01 21:09:14 +0200

    DC

--- src/app/SignUp/page.jsx
+++ src/app/SignUp/page.jsx
@@ -43,8 +43,8 @@ export default function Home() {
         <br />
 
         <div
-          className="w-full max-w-sm p-0 bg-transparent rounded-lg shadow sm:p-6 md:p-8 "
-        // className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 
+          className="w-[80%] p-0 bg-transparent rounded-lg shadow sm:p-6 md:p-8 "
+          // className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 
         // dark:bg-gray-800 dark:border-gray-700"
         >
           <form className="space-y-6" onSubmit={signUpHandler} >
