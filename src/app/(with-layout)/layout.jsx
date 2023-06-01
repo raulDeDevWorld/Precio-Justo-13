f8f265ec2d90da4e8406363aea53312ea3f13cd7
Your Name <you@example.com>
2023-06-01 20:29:27 +0200

    DC

--- src/app/(with-layout)/layout.jsx
+++ src/app/(with-layout)/layout.jsx
@@ -19,6 +19,7 @@ function Home({ children }) {
 
   const signOutHandler = () => {
     setUserProfile(null)
+    router.push('/')
     signOut()
   }
 
@@ -92,7 +93,7 @@ function Home({ children }) {
             </svg>
             <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Inicio</span>
           </button>
-          {user.rol === 'Distribuidor' && <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={() => redirectHandler(`/Distribuidor/Agregar`)}>
+          {user && user.rol === 'Distribuidor' && <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={() => redirectHandler(`/Distribuidor/Agregar`)}>
             <svg className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"></path>
             </svg>
